87e489e30e9c70e781db694e8a6fbba1bbfe32a9
조민성 <68271769+isord@users.noreply.github.com>
2026-08-31 03:15:13 +0900

    [Passed] 신기한 타일 뒤집기 (91ms, 21MB)

---
+++ trail2/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,23 @@
+MAX_V = 1000 * 100 * 2
+OFFSET = 1000 * 100
+
+tiles = [-1] * (MAX_V + 1)
+
+n = int(input())
+loc = OFFSET + 1
+
+for _ in range(n):
+    x, cmd = input().split()
+    x = int(x)
+    if cmd == 'L':
+        for i in range(loc, loc - x, -1):
+            tiles[i] = 1
+        loc -= x - 1
+    else:
+        for i in range(loc, loc + x):
+            tiles[i] = 0
+        loc += x - 1
+
+white = tiles.count(1)
+black = tiles.count(0)
+print(white, black)
