47c474dd8f3a98e8d3a221f76987f8d3aea24a65
조민성 <68271769+isord@users.noreply.github.com>
2026-09-09 22:00:07 +0900

    [Passed] 빙빙 돌며 수로 직사각형 채우기 (50ms, 16MB)

---
+++ trail2/빙빙 돌며 수로 직사각형 채우기/snail-number-square.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = [[0] * m for _ in range(n)]
+
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+r, c, d = 0, 0, 0
+
+for num in range(1, n * m + 1):
+    arr[r][c] = num
+
+    nr, nc = r + dr[d], c + dc[d]
+
+    if not (0 <= nr < n and 0 <= nc < m and arr[nr][nc] == 0):
+        d = (d + 1) % 4
+        nr, nc = r + dr[d], c + dc[d]
+
+    r, c = nr, nc
+
+for row in arr:
+    print(' '.join(map(str, row)))
